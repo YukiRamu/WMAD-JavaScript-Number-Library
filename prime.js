32f9f557a48e23f4552b4f6c7cd3a0d7980db0fd
Yuki <yuki_matsubara@outlook.com>
2021-03-18 09:32:31 -0700

    numbers.js updated

---
+++ prime.js
@@ -0,0 +1,5 @@
+/* Under construction */
+// For a number to be prime, it must meet the following conditions
+// Can it be divided by 1? - all numbers can be divided by one, so yes
+// can it be divided by itself? - all numbers, divided into themselves once
+// Can it be EVENLY divided by any other number? if it can, it can't be prime
